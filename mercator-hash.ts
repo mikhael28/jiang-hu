8eed09ea39d9cd6046a3cd7cb60dd1d286dbf69d
mikhael28 <mikhael@hey.com>
2025-08-26 18:09:23 -0700

    initial commit of utility files and readme pre npm package

---
+++ mercator-hash.ts
@@ -0,0 +1,505 @@
+export const mercator_hash = {
+    AND: [1.5218, 42.5], // Andorra
+    ARE: [53.97, 25.07], // United Arab Emirates
+    AFG: [66.93, 33.94], // Afghanistan
+    ATG: [-61.85, 17.06], // Antigua and Barbuda
+    AIA: [-62.97, 18.19], // Anguilla
+    ALB: [20.17, 41.31], // Albania
+    ARM: [45.04, 40.14], // Armenia
+    ANT: [-63.06, 12.22], // Netherlands Antilles
+    AGO: [17.87, -11.67],
+    ATA: [0, -90], // Antarctica (South Pole) - Special Case
+    ARG: [-63.66, -38.41], // Argentina
+    ASM: [-170.69, -40.89], // American Samoa
+    AUT: [16.37, 48.2], // Austria
+    AUS: [135, -25], // Australia
+    ABW: [-69.96, 12.5], // Aruba
+    ALA: [20.77, 60.11], // Åland Islands
+    AZE: [47.58, 40.14], // Azerbaijan
+    BIH: [18.42, 43.86],
+    BRB: [-59.61, 13.17], // Barbados
+    BGD: [90.37, 23.71], // Bangladesh
+    BEL: [4.35, 50.85], // Belgium
+    BFA: [-1.56, 12.35], // Burkina Faso
+    BGR: [23.32, 42.69], // Bulgaria
+    BHR: [50.58, 26.27], // Bahrain
+    BDI: [29.92, -3.3], // Burundi
+    BEN: [2.31, 9.3], // Benin
+    BLM: [-61.88, 17.3], // Saint Barthélemy
+    BMU: [-64.78, 32.3],
+    BRN: [114.88, 4.88], // Brunei
+    BOL: [-68.15, -16.5], // Bolivia
+    BES: [-68.25, 12.2], // Bonaire, Sint Eustatius and Saba
+    BRA: [-53.33, -15.78], // Brazil (approximate center)
+    BHS: [-77.8, 25.05], // Bahamas
+    BTN: [90.43, 27.47], // Bhutan
+    BVT: [0, -54.47], // Bouvet Island (tiny, near South Pole)
+    BWA: [24.69, -22.33], // Botswana
+    BLR: [27.54, 53.89], // Belarus
+    BLZ: [-88.97, 17.25],
+    CAN: [-95.71, 54.17], // Canada
+    CCK: [121.67, -12.17], // Cocos (Keeling) Islands
+    COD: [21.75, -4.04], // Democratic Republic of the Congo
+    CAF: [19.5, 4.22], // Central African Republic
+    COG: [15.04, -1.9], // Republic of the Congo
+    CHE: [8.23, 46.81], // Switzerland
+    CIV: [-5.29, 8.03], // Côte d'Ivoire
+    COK: [-157.5, -21.23], // Cook Islands
+    CHL: [-70.64, -33.47], // Chile
+    CMR: [12.36, 7.35], // Cameroon
+    CHN: [103.83, 35.86], // China (you already have this)
+    COL: [-74.24, 4.57], // Colombia,
+    CRI: [-83.78, 10.0], // Costa Rica
+    CUB: [-82.35, 23.11], // Cuba
+    CPV: [-23.51, 16.0], // Cabo Verde
+    CUW: [-69.01, 12.18], // Curaçao
+    CXR: [121.67, -16.86], // Christmas Island
+    CYP: [33.33, 35.12], // Cyprus
+    CZE: [15.47, 50.08], // Czech Republic
+    DEU: [10.45, 51.0], // Germany
+    DJI: [42.5, 11.5], // Djibouti
+    DNK: [9.5, 56.25], // Denmark
+    DMA: [-61.37, 15.29], // Dominica
+    DOM: [-70.16, 18.47], // Dominican Republic
+    DZA: [3.18, 28.03], // Algeria
+    ECU: [-78.68, -1.83], // Ecuador
+    EST: [24.75, 59.43], // Estonia
+    EGY: [30, 30], // Egypt (approximate center)
+    ESH: [-13, 28], // Western Sahara (claimed territory)
+    ERI: [38.97, 15.17], // Eritrea
+    ESP: [-3.7, 40.4], // Spain
+    ETH: [38.77, 9.0], // Ethiopia
+    FIN: [26, 62], // Finland
+    FJI: [180, -18.14], // Fiji
+    FLK: [-59.51, -51.7], // Falkland Islands
+    FSM: [166.8, 7.42], // Micronesia
+    FRO: [-6.91, 62.1], // Faroe Islands
+    FRA: [2.35, 48.86], // France
+    GAB: [11.76, 0.4], // Gabon
+    GBR: [-3.48, 52.35], // United Kingdom
+    GRD: [-61.67, 15.49], // Grenada
+    GEO: [43.68, 41.71], // Georgia
+    GUF: [-53.18, 3.93], // French Guiana
+    GGY: [-2.59, 49.46], // Guernsey
+    GHA: [-1.02, 8.5], // Ghana
+    GIB: [-5.35, 36.14], // Gibraltar
+    GRL: [-39.07, 64.18], // Greenland
+    GMB: [-15.5, 13.47], // Gambia
+    GIN: [-9.5, 10], // Guinea
+    GLP: [-61.5, 16.25], // Guadeloupe
+    GNQ: [11.7, 1.5], // Equatorial Guinea
+    GRC: [23.72, 39.01], // Greece
+    SGS: [-36, -90], // South Georgia and the South Sandwich Islands (approximate center near South Pole)
+    GTM: [-90.55, 15.79], // Guatemala
+    GUM: [144.8, 13.47], // Guam
+    GNB: [-15.5, 12.4], // Guinea-Bissau
+    GUY: [-58.16, 5.0], // Guyana
+    HKG: [114.17, 22.3], // Hong Kong
+    HMD: [73.55, -53.09], // Heard Island and McDonald Islands
+    HND: [-86.24, 15.19], // Honduras
+    HRV: [15.99, 45.81], // Croatia
+    HTI: [-72.29, 18.97], // Haiti
+    HUN: [19.5, 47.17], // Hungary
+    IDN: [117.67, -5.33], // Indonesia (approximate center of major islands)
+    IRL: [-8.21, 53.41], // Ireland
+    ISR: [34.85, 31.95], // Israel
+    IMN: [-4.48, 54.23], // Isle of Man
+    IND: [80, 25], // India (approximate center)
+    IOT: [72.34, -60.18], // British Indian Ocean Territory (scattered islands)
+    IRQ: [43.61, 33.25], // Iraq
+    IRN: [53.69, 32.42], // Iran
+    ISL: [-19.02, 64.13], // Iceland
+    ITA: [12.37, 41.9], // Italy
+    JEY: [-2.1, 49.22], // Jersey
+    JAM: [-76.79, 18.11], // Jamaica
+    JOR: [38.76, 31.3], // Jordan
+    JPN: [139.75, 35.68], // Japan
+    KEN: [37.98, -0.02], // Kenya
+    KGZ: [74.6, 41.2], // Kyrgyzstan
+    KHM: [104.99, 12.56], // Cambodia
+    KIR: [177.4, -3.9], // Kiribati
+    COM: [43.87, 12.16], // Comoros
+    KNA: [-62.74, 17.35], // Saint Kitts and Nevis
+    PRK: [127.5, 40.0], // North Korea (approximate center)
+    KOR: [127.76, 35.9], // South Korea
+    KWT: [47.58, 29.31], // Kuwait
+    CYM: [-81.25, 19.5], // Cayman Islands
+    KAZ: [68.47, 48.01], // Kazakhstan
+    LAO: [102.41, 17.9], // Laos
+    LBN: [35.86, 33.88], // Lebanon
+    LCA: [-61.13, 13.93], // Saint Lucia
+    LIE: [9.55, 47.16], // Liechtenstein
+    LKA: [80.77, 7.87], // Sri Lanka
+    LBR: [-9.54, 6.43], // Liberia
+    LSO: [28.3, -29.61], // Lesotho
+    LTU: [23.88, 55.17], // Lithuania
+    LUX: [6.13, 49.61], // Luxembourg
+    LVA: [24.6, 56.87], // Latvia
+    LBY: [17.29, 26.92], // Libya
+    MAR: [-5.76, 31.79], // Morocco
+    MCO: [7.42, 43.73], // Monaco
+    MDA: [28.36, 47.41], // Moldova
+    MNE: [19.26, 42.5], // Montenegro
+    MAF: [-55.67, 17.9], // Saint Martin (French part)
+    MDG: [47.5, -20.5], // Madagascar
+    MHL: [171.18, 7.14], // Marshall Islands
+    MKD: [21.74, 41.6], // North Macedonia
+    MLI: [-5, 17], // Mali
+    MMR: [95.97, 19.76], // Myanmar
+    MNG: [104.17, 46.86], // Mongolia
+    MAC: [114.17, 22.18], // Macau
+    MNP: [145.43, 17.33], // Northern Mariana Islands
+    MTQ: [-61.05, 14.6], // Martinique
+    MRT: [-15.55, 18.07], // Mauritania
+    MSR: [-62.18, 17.78], // Montserrat
+    MLT: [14.48, 35.88], // Malta
+    MUS: [57.57, -20.34], // Mauritius
+    MDV: [73.0, 4.2], // Maldives
+    MWI: [34.39, -13.25], // Malawi
+    MEX: [-102.55, 23.63], // Mexico (approximate center)
+    MYS: [112.37, 3.15], // Malaysia
+    MOZ: [35.32, -18.66], // Mozambique
+    NAM: [18.49, -22.95], // Namibia
+    NCL: [166.17, -21.3], // New Caledonia
+    NER: [10, 15], // Niger
+    NFK: [-177.12, -29.04], // Norfolk Island
+    NGA: [8, 10], // Nigeria (approximate center)
+    NIC: [-85.2, 12.13], // Nicaragua
+    NLD: [5.29, 52.38], // Netherlands
+    NOR: [10.75, 60.47], // Norway
+    NPL: [84.14, 28.39], // Nepal
+    NRU: [166.92, -0.52], // Nauru
+    NIU: [-170.62, -19.05], // Niue
+    NZL: [174.88, -40.9], // New Zealand
+    OMN: [55.93, 21.51], // Oman
+    PAN: [-80.78, 8.53], // Panama
+    PER: [-70.63, -9.19], // Peru
+    PYF: [-149.6, -25.04], // French Polynesia
+    PNG: [147.1, -6.3], // Papua New Guinea
+    PHL: [121.77, 12.87], // Philippines
+    PAK: [73.19, 30.37], // Pakistan
+    POL: [19.14, 52.19], // Poland
+    SPM: [-55.05, 47.05], // Saint Pierre and Miquelon
+    PCN: [-166.27, -24.3], // Pitcairn Islands
+    PRI: [-66.45, 18.25], // Puerto Rico
+    PSE: [35.24, 31.95], // Palestine
+    PRT: [-8.22, 39.38], // Portugal
+    PLW: [134.5, 7.5], // Palau
+    PRY: [-58.44, -23.44], // Paraguay
+    QAT: [51.5, 25.3], // Qatar
+    REU: [55.53, -21.1], // Réunion
+    ROU: [26.82, 46.24], // Romania
+    SRB: [21, 44], // Serbia
+    RUS: [100, 60], // Russia (approximate center, vast country)
+    RWA: [30.06, -1.95], // Rwanda
+    SAU: [45.07, 23.68], // Saudi Arabia
+    SLB: [160.15, -9.58], // Solomon Islands
+    SYC: [55.69, -4.67], // Seychelles
+    SDN: [30, 15], // Sudan (approximate center, large country)
+    SWE: [18.64, 60.12], // Sweden
+    SGP: [103.8, 1.35], // Singapore
+    SHN: [-13.81, -37.88], // Saint Helena, Ascension and Tristan da Cunha
+    SVN: [15.01, 46.15], // Slovenia
+    SJM: [-12.38, 74.42], // Svalbard and Jan Mayen
+    SVK: [19.69, 48.15], // Slovakia
+    SLE: [-11.74, 8.48], // Sierra Leone
+    SMR: [12.45, 43.94], // San Marino
+    SEN: [-14.4, 14.7], // Senegal
+    SOM: [46.19, 5.13], // Somalia
+    SUR: [-56.03, 4.04], // Suriname
+    SSS: [31.86, 4.85], // South Sudan (approximate center)
+    STP: [-17.55, 0.18], // São Tomé and Príncipe
+    SLV: [-88.97, 13.7], // El Salvador
+    SXM: [-63.06, 18.08], // Sint Maarten (Dutch part)
+    SYR: [38.69, 34.8], // Syria
+    SZW: [31.39, -26.31], // Eswatini (Swaziland)
+    TCA: [-69.72, 21.78], // Turks and Caicos Islands
+    TCD: [19, 15], // Chad (approximate center, large country)
+    ATF: [-51.9, 43.9], // French Southern and Antarctic Territories (scattered islands)
+    TGO: [1.2, 8.62], // Togo
+    THA: [100.99, 13.75], // Thailand
+    TJK: [71.28, 38.86], // Tajikistan
+    TKL: [-172.88, -8.9], // Tokelau
+    TLS: [125.76, -8.87], // Timor-Leste
+    TKM: [58.55, 39.93], // Turkmenistan
+    TUN: [10.17, 34.23], // Tunisia
+    TON: [-174.8, -21.17], // Tonga
+    TUR: [35.24, 38.96], // Turkey
+    TTO: [-60.67, 10.69], // Trinidad and Tobago
+    TUV: [178.14, -7.1], // Tuvalu
+    TZA: [35, -6], // Tanzania (approximate center, large country)
+    UKR: [38.94, 50.45], // Ukraine
+    UGA: [32.45, 1.37], // Uganda
+    UMI: [-171.8, 18.11], // United States Minor Outlying Islands (scattered islands)
+    USA: [-95.71, 37.09], // United States (approximate center)
+    URY: [-55.79, -32.39], // Uruguay
+    UZB: [69.29, 41.37], // Uzbekistan
+    VAT: [12.46, 41.9], // Vatican City
+    VCT: [-61.2, 13.17], // Saint Vincent and the Grenadines
+    VEN: [-66.9, 10.48], // Venezuela
+    VGB: [-64.62, 18.11], // British Virgin Islands
+    VIR: [-64.89, 18.33], // U.S. Virgin Islands
+    VNM: [105.8, 21.03], // Vietnam
+    VUT: [168.65, -17.75], // Vanuatu
+    WLF: [-176.12, -14.23], // Wallis and Futuna
+    WSM: [-171.83, -13.83], // Samoa
+    YEM: [48.51, 15.5], // Yemen
+    MYT: [45.14, 12.82], // Mayotte (French overseas department)
+    ZAF: [28.31, -25.74], // South Africa
+    ZMB: [27.84, -13.13], // Zambia
+    ZWE: [31.04, -19.02], // Zimbabwe
+  };
+  
+  export const countrySizeScales = {
+    ABW: 1800, // Aruba
+    AFG: 600, // Afghanistan
+    AGO: 500, // Angola
+    AIA: 1800, // Anguilla
+    ALA: 1200, // Åland Islands
+    ALB: 1200, // Albania
+    AND: 2000, // Andorra
+    ANT: 1800, // Netherlands Antilles
+    ARE: 700, // United Arab Emirates
+    ARG: 450, // Argentina
+    ARM: 1200, // Armenia
+    ASM: 1600, // American Samoa
+    ATA: 400, // Antarctica
+    ATF: 600, // French Southern and Antarctic Territories
+    ATG: 1800, // Antigua and Barbuda
+    AUS: 350, // Australia
+    AUT: 900, // Austria
+    AZE: 1100, // Azerbaijan
+    BDI: 1200, // Burundi
+    BEL: 1000, // Belgium
+    BEN: 900, // Benin
+    BES: 1800, // Bonaire, Sint Eustatius and Saba
+    BFA: 700, // Burkina Faso
+    BGD: 700, // Bangladesh
+    BGR: 900, // Bulgaria
+    BHR: 1800, // Bahrain
+    BHS: 800, // Bahamas
+    BIH: 900, // Bosnia and Herzegovina
+    BLM: 1800, // Saint Barthélemy
+    BLR: 800, // Belarus
+    BLZ: 1300, // Belize
+    BMU: 1800, // Bermuda
+    BOL: 500, // Bolivia
+    BRA: 350, // Brazil
+    BRB: 1800, // Barbados
+    BRN: 1300, // Brunei
+    BTN: 1200, // Bhutan
+    BVT: 1500, // Bouvet Island
+    BWA: 700, // Botswana
+    CAF: 600, // Central African Republic
+    CAN: 300, // Canada
+    CCK: 1800, // Cocos (Keeling) Islands
+    CHE: 900, // Switzerland
+    CHL: 450, // Chile
+    CHN: 350, // China
+    CIV: 700, // Côte d'Ivoire
+    CMR: 700, // Cameroon
+    COD: 500, // Democratic Republic of the Congo
+    COG: 700, // Republic of the Congo
+    COK: 1600, // Cook Islands
+    COL: 500, // Colombia
+    COM: 1600, // Comoros
+    CPV: 1200, // Cabo Verde
+    CRI: 1000, // Costa Rica
+    CUB: 800, // Cuba
+    CUW: 1800, // Curaçao
+    CXR: 1800, // Christmas Island
+    CYM: 1600, // Cayman Islands
+    CYP: 1500, // Cyprus
+    CZE: 900, // Czech Republic
+    DEU: 800, // Germany
+    DJI: 1200, // Djibouti
+    DMA: 1800, // Dominica
+    DNK: 900, // Denmark
+    DOM: 900, // Dominican Republic
+    DZA: 500, // Algeria
+    ECU: 700, // Ecuador
+    EGY: 600, // Egypt
+    ERI: 1000, // Eritrea
+    ESH: 700, // Western Sahara
+    ESP: 700, // Spain
+    EST: 1100, // Estonia
+    ETH: 600, // Ethiopia
+    FIN: 700, // Finland
+    FJI: 900, // Fiji
+    FLK: 800, // Falkland Islands
+    FRA: 700, // France
+    FRO: 1400, // Faroe Islands
+    FSM: 1000, // Micronesia
+    GAB: 800, // Gabon
+    GBR: 800, // United Kingdom
+    GEO: 1100, // Georgia
+    GGY: 1800, // Guernsey
+    GHA: 800, // Ghana
+    GIB: 2200, // Gibraltar
+    GIN: 800, // Guinea
+    GLP: 1400, // Guadeloupe
+    GMB: 1200, // Gambia
+    GNB: 1100, // Guinea-Bissau
+    GNQ: 1200, // Equatorial Guinea
+    GRC: 900, // Greece
+    GRD: 1800, // Grenada
+    GRL: 250, // Greenland
+    GTM: 900, // Guatemala
+    GUF: 900, // French Guiana
+    GUM: 1600, // Guam
+    GUY: 700, // Guyana
+    HKG: 1600, // Hong Kong
+    HMD: 1200, // Heard Island and McDonald Islands
+    HND: 900, // Honduras
+    HRV: 1100, // Croatia
+    HTI: 1300, // Haiti
+    HUN: 900, // Hungary
+    IDN: 500, // Indonesia
+    IMN: 1600, // Isle of Man
+    IND: 400, // India
+    IOT: 1800, // British Indian Ocean Territory
+    IRL: 900, // Ireland
+    IRN: 600, // Iran
+    IRQ: 800, // Iraq
+    ISL: 700, // Iceland
+    ISR: 1300, // Israel
+    ITA: 800, // Italy
+    JAM: 1400, // Jamaica
+    JEY: 1800, // Jersey
+    JOR: 1100, // Jordan
+    JPN: 700, // Japan
+    KAZ: 500, // Kazakhstan
+    KEN: 600, // Kenya
+    KGZ: 800, // Kyrgyzstan
+    KHM: 800, // Cambodia
+    KIR: 1200, // Kiribati
+    KNA: 1800, // Saint Kitts and Nevis
+    KOR: 900, // South Korea
+    KWT: 1400, // Kuwait
+    LAO: 800, // Laos
+    LBN: 1400, // Lebanon
+    LBR: 1000, // Liberia
+    LBY: 600, // Libya
+    LCA: 1800, // Saint Lucia
+    LIE: 1800, // Liechtenstein
+    LKA: 1000, // Sri Lanka
+    LSO: 1300, // Lesotho
+    LTU: 1100, // Lithuania
+    LUX: 1800, // Luxembourg
+    LVA: 1100, // Latvia
+    MAC: 1800, // Macau
+    MAF: 1800, // Saint Martin
+    MAR: 600, // Morocco
+    MCO: 2500, // Monaco
+    MDA: 1100, // Moldova
+    MDG: 600, // Madagascar
+    MDV: 1800, // Maldives
+    MEX: 500, // Mexico
+    MHL: 1400, // Marshall Islands
+    MKD: 1200, // North Macedonia
+    MLI: 500, // Mali
+    MLT: 2000, // Malta
+    MMR: 600, // Myanmar
+    MNE: 1300, // Montenegro
+    MNG: 500, // Mongolia
+    MNP: 1600, // Northern Mariana Islands
+    MOZ: 500, // Mozambique
+    MRT: 600, // Mauritania
+    MSR: 1800, // Montserrat
+    MTQ: 1600, // Martinique
+    MUS: 1600, // Mauritius
+    MWI: 900, // Malawi
+    MYS: 600, // Malaysia
+    MYT: 1600, // Mayotte
+    NAM: 600, // Namibia
+    NCL: 1200, // New Caledonia
+    NER: 500, // Niger
+    NFK: 1800, // Norfolk Island
+    NGA: 500, // Nigeria
+    NIC: 900, // Nicaragua
+    NIU: 1800, // Niue
+    NLD: 900, // Netherlands
+    NOR: 800, // Norway
+    NPL: 900, // Nepal
+    NRU: 2000, // Nauru
+    NZL: 700, // New Zealand
+    OMN: 600, // Oman
+    PAK: 500, // Pakistan
+    PAN: 900, // Panama
+    PCN: 1800, // Pitcairn Islands
+    PER: 500, // Peru
+    PHL: 600, // Philippines
+    PLW: 1600, // Palau
+    PNG: 700, // Papua New Guinea
+    POL: 800, // Poland
+    PRI: 1200, // Puerto Rico
+    PRK: 900, // North Korea
+    PRT: 1000, // Portugal
+    PRY: 600, // Paraguay
+    PSE: 1400, // Palestine
+    PYF: 1000, // French Polynesia
+    QAT: 1400, // Qatar
+    REU: 1600, // Réunion
+    ROU: 800, // Romania
+    RUS: 250, // Russia
+    RWA: 1200, // Rwanda
+    SAU: 500, // Saudi Arabia
+    SDN: 600, // Sudan
+    SEN: 800, // Senegal
+    SGP: 2000, // Singapore
+    SGS: 700, // South Georgia and the South Sandwich Islands
+    SHN: 1600, // Saint Helena
+    SJM: 1000, // Svalbard and Jan Mayen
+    SLB: 1000, // Solomon Islands
+    SLE: 1100, // Sierra Leone
+    SLV: 1300, // El Salvador
+    SMR: 2200, // San Marino
+    SOM: 600, // Somalia
+    SPM: 1600, // Saint Pierre and Miquelon
+    SRB: 900, // Serbia
+    SSS: 600, // South Sudan
+    STP: 1800, // São Tomé and Príncipe
+    SUR: 800, // Suriname
+    SVK: 900, // Slovakia
+    SVN: 1200, // Slovenia
+    SWE: 700, // Sweden
+    SWZ: 1300, // Eswatini (Swaziland)
+    SXM: 1800, // Sint Maarten
+    SYC: 1600, // Seychelles
+    SYR: 800, // Syria
+    TCA: 1600, // Turks and Caicos Islands
+    TCD: 500, // Chad
+    TGO: 1100, // Togo
+    THA: 600, // Thailand
+    TJK: 900, // Tajikistan
+    TKL: 1800, // Tokelau
+    TKM: 700, // Turkmenistan
+    TLS: 1200, // Timor-Leste
+    TON: 1600, // Tonga
+    TTO: 1600, // Trinidad and Tobago
+    TUN: 1000, // Tunisia
+    TUR: 700, // Turkey
+    TUV: 2000, // Tuvalu
+    TWN: 1100, // Taiwan
+    TZA: 600, // Tanzania
+    UGA: 800, // Uganda
+    UKR: 700, // Ukraine
+    UMI: 1500, // United States Minor Outlying Islands
+    URY: 900, // Uruguay
+    USA: 300, // United States
+    UZB: 700, // Uzbekistan
+    VAT: 2500, // Vatican City
+    VCT: 1800, // Saint Vincent and the Grenadines
+    VEN: 600, // Venezuela
+    VGB: 1800, // British Virgin Islands
+    VIR: 1800, // U.S. Virgin Islands
+    VNM: 600, // Vietnam
+    VUT: 1200, // Vanuatu
+    WLF: 1600, // Wallis and Futuna
+    WSM: 1600, // Samoa
+    YEM: 600, // Yemen
+    ZAF: 700, // South Africa
+    ZMB: 700, // Zambia
+    ZWE: 800, // Zimbabwe
+  };
+  
